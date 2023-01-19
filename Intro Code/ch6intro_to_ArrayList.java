28843426d1b870d110a92c90ea27ff2a504f6cb8
Kevin Hayes <khayes@naperville203.org>
2023-01-19 11:22:07 -0600

    cleaned up notes

--- Intro Code/ch6intro_to_ArrayList.java
+++ Intro Code/ch6intro_to_ArrayList.java
@@ -16,65 +16,65 @@ import java.util.ArrayList;//yes AL are in util
 public class ch6intro_to_ArrayList
 
 {
-	final static int MAX = 20;
+    final static int MAX = 20;
 
 public static void main (String[] args)
 {
-	//ArrayList notes
-/*
-	// <> indicates the type of ArrayList
-	ArrayList <String> farm = new ArrayList<>();
+    //ArrayList notes
+    /**/
+    // <> indicates the type of ArrayList
+    ArrayList <String> farm = new ArrayList<>();
 
-	farm.add("cow");farm.add("chicken");
-	for (int i = 0; i<20;i++)
-	{
-		farm.add("cow");
-		farm.add("chicken");
-		farm.add("chicken");
-		farm.add("duck");
-		farm.add("horse");
+    farm.add("cow");farm.add("chicken");
+    for (int i = 0; i<20;i++)
+    {
+        farm.add("cow");
+        farm.add("chicken");
+        farm.add("chicken");
+        farm.add("duck");
+        farm.add("horse");
 
-	}
-	farm.add("dog");farm.add("dog");farm.add("dog");
-	farm.add("pig");farm.add("pig");farm.add("pig");
-	farm.add("pig");farm.add("pig");farm.add("pig");
+    }
+    farm.add("dog");farm.add("dog");farm.add("dog");
+    farm.add("pig");farm.add("pig");farm.add("pig");
+    farm.add("pig");farm.add("pig");farm.add("pig");
 
-	System.out.println(farm);
+    System.out.println(farm);
+    /*
+    int count = 0;
+    for (int i = 0; i<farm.size();i++)
+    {
+        if(farm.get(i).equals("chicken"))
+            count++;
 
-	int count = 0;
-	for (int i = 0; i<farm.size();i++)
-	{
-		if(farm.get(i).equals("chicken"))
-			count++;
-
-		if (farm.get(i).equals("chicken"))
-			farm.remove(i);
+        if (farm.get(i).equals("chicken"))
+            farm.remove(i);
 
 
-	}
+    }
 
-	System.out.println("There are " + count+ " chickens");
+    System.out.println("There are " + count+ " chickens");
 
-	System.out.println(farm);
+    System.out.println(farm);
+    /*
+    ArrayList <String> list = new ArrayList<>();
 
-	ArrayList <String> list = new ArrayList<>();
+    //ArrayList <int> list1 = new ArrayList<>();// no primitive data types
 
-	//ArrayList <int> list1 = new ArrayList<>();// no primitive data types
+    ArrayList <Integer> list1 = new ArrayList<>();
 
-	ArrayList <Integer> list1 = new ArrayList<>();
+    ArrayList <CD> mycds = new ArrayList<>();
 
-	ArrayList <CD> mycds = new ArrayList<>();
+    for(int i = 0; i<3; i++)
+    {
 
-	//for(int i = 0; i<3; i++)
-	//{
+    mycds.add(new CD("Wilco","Jeff Tweety",12.99,16);
+    mycds.add(new CD("Throwup","Britiny Spears",12.99,16);
+    mycds.add(new CD("Oxygen","CatFish and the Bottlemen",12.99,16);
 
-	mycds.add(new CD("Wilco","Jeff Tweety",12.99,16);
-	mycds.add(new CD("Throwup","Britiny Spears",12.99,16);
-	mycds.add(new CD("Oxygen","CatFish and the Bottlemen",12.99,16);
+    System.out.print(mycds);
 
-	System.out.print(mycds);
-
-	//}
+    }/* */
 
 }// end of main
 
