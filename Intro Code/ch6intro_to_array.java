f5d13b2a34955dd6fd651ec2ae2441ce48bc3940
cpatterson367 <cpatterson@stu.naperville203.org>
2026-02-04 11:58:46 -0600

    shity

--- Intro Code/ch6intro_to_array.java
+++ Intro Code/ch6intro_to_array.java
@@ -16,143 +16,148 @@ import java.util.*;
 public class ch6intro_to_array
 
 {
-	final static int MAX = 20;
+    final static int MAX = 20;
 
-public static void main (String[] args)
-{
+    public static void main (String[] args)
+    {
+    
+        /**/
+            mathy useful = new mathy();
+            int x = 50;
+            int[] num = new int[20];
+            int[] values = new int[MAX];
+            //.length not a method public data
+            int[] oddnum = new int[num.length];
+            int[] evennum = new int[num.length];
+            int[] negnum = new int[MAX];
+            
+            int[] nums = {1,3,4,6,8,2,4,7,9};
+           Random gen = new Random();
+           for (int i = 0; i<nums.length;i++)
+           {
+               System.out.print(nums[i]);
+           }
+        /*
+    
+            System.out.println(Searches.linearSearch(num,look)  );
+            Sorts.selectionSort(num);
+            System.out.println(Searches.binarySearch(num,look)  );
+    
+    
+    
+            //for each loop
+        /*
+    
+            useful.getOdd(num,oddnum);
+    
+    
+    
+    
+            //System.out.println("----------SORTING-----------"  );
+    
+    
+    
+    
+    
+    
+        /*
+            System.out.println("Enter a number");
+            int find = Keyboard.readInt();
+    
+            System.out.println(Searches. binarySearch(num,find));
+    
+        /*
+    
+    
+    
+    
+    
+    
+    
+        /*
+            //make a two dementional array
+            int [][] table = new int [5][10];
+    
+            // Load the table with values
+            for (int row=0; row < table.length; row++)
+               for (int col=0; col < table[row].length; col++)
+                 table[row][col] = row * 10 + col;
+    
+            //print the table
+             for (int row=0; row < table.length; row++)
+             {
+                 for (int col=0; col < table[row].length; col++)
+                      System.out.print (table[row][col] + "\t");
+    
+                    System.out.println();
+               }
+    
+    
+        // make a 2d array with an iniitalizer list.
+        //print the length of row and col
+        /*
+        int [][] scores = { {1,2,3},//6
+                            {2,2,3},//7
+                            {3,2,3},//8
+                            {4,2,3}    };//9
+    
+        //how many columns
+        System.out.println(scores[0].length+"col");
+        //how many rows
+        System.out.println(scores.length+"row");
+    
+        //sum entire 2D array
+        System.out.print("The sum of this array is" );
+    
+        //sum a row of 2D array
+        System.out.print("The sum of row "));
+    
+    
+    
+    
+    
+    
+    
+        /**/
+    }// end of main
 
-/**/
-		mathy useful = new mathy();
-		int x = 50;
-		int[] num = new int[20];
-		int[] values = new int[MAX];
-	    //.length not a method public data
-	    int[] oddnum = new int[num.length];
-	    int[] evennum = new int[num.length];
-	    int[] negnum = new int[MAX];
+    public static int[] doubleSize(int[] array)
+    {
+    
+    
+        return array;
+    
+    
+    }
 
-	   Random gen = new Random();
-/*
-
-		System.out.println(Searches.linearSearch(num,look)  );
-		Sorts.selectionSort(num);
-		System.out.println(Searches.binarySearch(num,look)  );
-
+    //passing int by value
+    public static void foo(int x)
+    {
+    
+       x = 100;
+    }
 
 
-		//for each loop
-/*
-
-		useful.getOdd(num,oddnum);
-
-
-
-
-		//System.out.println("----------SORTING-----------"  );
-
-
-
-
-
-
-/*
-		System.out.println("Enter a number");
-		int find = Keyboard.readInt();
-
-		System.out.println(Searches. binarySearch(num,find));
-
-   /*
-
-
-
-
-
-
-
- /*
- 	   //make a two dementional array
- 	   int [][] table = new int [5][10];
-
- 	   // Load the table with values
- 	   for (int row=0; row < table.length; row++)
- 	      for (int col=0; col < table[row].length; col++)
-             table[row][col] = row * 10 + col;
-
- 	   //print the table
- 	    for (int row=0; row < table.length; row++)
- 	    {
- 			for (int col=0; col < table[row].length; col++)
- 	         	System.out.print (table[row][col] + "\t");
-
- 	   		System.out.println();
-       	}
-
-
-	// make a 2d array with an iniitalizer list.
-	//print the length of row and col
-/*
-	int [][] scores = { {1,2,3},//6
-						{2,2,3},//7
-						{3,2,3},//8
-						{4,2,3}    };//9
-
-	//how many columns
-	System.out.println(scores[0].length+"col");
-	//how many rows
-	System.out.println(scores.length+"row");
-
-	//sum entire 2D array
-	System.out.print("The sum of this array is" );
-
-	//sum a row of 2D array
-	System.out.print("The sum of row "));
-
-
-
-
-
-
-
-/**/
-}// end of main
-
-   public static int[] doubleSize(int[] array)
-   {
-
-
-		return array;
-
-
-   }
-
-//passing int by value
-   public static void foo(int x)
-   {
-
-	   x = 100;
-   }
-
-
-//arrays are passed by reference
+    //arrays are passed by reference
     public static void foo(int [] num)
       {
 
-		  num[0] = 100;
+          num[0] = 100;
 
       }
 
-	/*
-
-
-   public static void odd()
-   {
-
-	   int count =0;
-
-
-   }// end of odd
-
-	/*  */
+    /*
+    
+    
+    public static void odd()
+    {
+    
+        int count =0;
+    
+    
+    }// end of odd
+    
+    /*  */
 }// end of class
 
 
