9e9a234dca8b003136bc4b3c418f0dd3a2b6656d
cpatterson367 <cpatterson@stu.naperville203.org>
2026-03-02 11:24:35 -0600

    wrodl

--- ArrayMethods/Word.java
+++ ArrayMethods/Word.java
@@ -18,22 +18,24 @@ public class Word
         
         ArrayList<String> wordList = new ArrayList<>();
         
+        String last = "a";
         while(scanFile.hasNext())
         {
+            boolean adjDupe = false, flag = false;;
+            
             String word = scanFile.next();
-            boolean flag = false;
+            if (word.equals(last))
+                 adjDupe = true;
+            last = word;
+            
             for (int i = 1; i < word.length(); i++)
-            {
-                if (word.charAt(i) == word.charAt(i-1))
-                    flag = true;
-            }
+                if (word.charAt(i) == 'i' || word.charAt(i) == 'a' || word.charAt(i) == 'u')
+                    if (word.charAt(i) == word.charAt(i-1))
+                        flag = true;
             
-            
-            if (word.length() == 6 && flag == false)
+            if (word.length() == 6 && flag == false && adjDupe == false)
                 wordList.add(word);
-                
-                
         }
-        System.out.println(wordList.size()); // = 29880
+        System.out.println(wordList.size()); // = 29880 no removal
     }
 }
